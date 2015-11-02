8995ab400ca645b4fc4257b6299d69b22a129015
Lukas <lukas.blumer@uzh.ch>
2015-11-02 12:32:12 +0100

    latest version, adjusting for calling from Moviebase

--- src/amdb/client/Map.java
+++ src/amdb/client/Map.java
@@ -11,62 +11,26 @@ import com.googlecode.gwt.charts.client.ChartLoader;
 import com.googlecode.gwt.charts.client.ChartPackage;
 import com.googlecode.gwt.charts.client.ColumnType;
 import com.googlecode.gwt.charts.client.DataTable;
+import com.googlecode.gwt.charts.client.event.SelectEvent;
+import com.googlecode.gwt.charts.client.event.SelectHandler;
 import com.googlecode.gwt.charts.client.geochart.GeoChart;
 import com.googlecode.gwt.charts.client.geochart.GeoChartColorAxis;
 import com.googlecode.gwt.charts.client.geochart.GeoChartOptions;
 import com.googlecode.gwt.charts.client.geochart.MagnifyingGlass;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
-public class Map implements EntryPoint {
-	private Button addStockButton = new Button("Lukas");
-	private SplitLayoutPanel sPanel = new SplitLayoutPanel(5);
+import amdb.shared.MovieCollection;
+import amdb.shared.MovieCollectionConverter;
+
+public class Map {
+	// create constructor
 	
-	
-	/**
-	 * This is the entry point method. (this goes once it's tested and implemented properly)
-	 */
-	public void onModuleLoad() {
-		
-		ChartLoader chartLoader = new ChartLoader(ChartPackage.CORECHART);
-		 chartLoader.loadApi(new Runnable() {
-		        @Override
-		        public void run() {
-		        	DataTable table = DataTable.create();
-					drawMap(sPanel, table);
-					
-		        }
-		 });
-		 sPanel.addWest(addStockButton, 10);
-		 RootPanel.get("MainContent").add(sPanel);
-	}
-	
-	public void drawMap(SplitLayoutPanel panel, MovieCollection collection) {
+	public void drawMap(MovieCollection collection) {
 		GeoChart geoChart;
 		geoChart = new GeoChart();
-		panel.add(geoChart);
+		// needs to be added to center panel
 		
-		// Convert array to dataTable
-		
-		// here goes conversion of database to dataTable
-		
-		
-		// this is test stuff
-		DataTable dataTable = DataTable.create();
-		dataTable.addColumn(ColumnType.STRING, "Country");
-		dataTable.addColumn(ColumnType.NUMBER, "Movies");
-		dataTable.addRows(7);
-		dataTable.setValue(0, 0, "Switzerland");
-		dataTable.setValue(0, 1, 300);
-		dataTable.setValue(1, 0, "Austria");
-		dataTable.setValue(1, 1, 400);
-		dataTable.setValue(2, 0, "United States");
-		dataTable.setValue(2, 1, 800);
-		dataTable.setValue(3, 0, "Sweden");
-		dataTable.setValue(3, 1, 200);
-		dataTable.setValue(4, 0, "United Kingdom");
-		dataTable.setValue(4, 1, 700);
+		// Conversion of MovieCollection to DataTable
+		DataTable dataTable = MovieCollectionConverter.toDataTableCountryAmount(collection);
 		
 		// Color countries according to number of movies released
 		GeoChartOptions options = GeoChartOptions.create();
@@ -79,11 +43,11 @@ public class Map implements EntryPoint {
 		// onClick event listener (cannot figure out how to do this)
 		geoChart.addSelectHandler(new SelectHandler() {
 			public void onSelect(SelectEvent select) {
-				addFilter();
+				// addFilter();
 			}
 		});
 		
-		// zoom (no idea if this works)
+		// zoom (no idea if this works) - probably not
 		MagnifyingGlass zoom = MagnifyingGlass.create();
 		zoom.setEnable(true);
 		zoom.setZoomFactor(1000.0);
