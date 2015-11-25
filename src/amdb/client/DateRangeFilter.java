35653ba6109c10d31dbc03bca6296e5d6c42e936
sflin <selin.fabel@uzh.ch>
2015-11-25 14:55:59 +0100

    created class for DateRangeFilter
    as git desktop has killed itself I'm trying some things to commit my files...
    - DateRangeFilter class - doesn't work yet

---
+++ src/amdb/client/DateRangeFilter.java
@@ -0,0 +1,42 @@
+package amdb.client;
+
+import com.googlecode.gwt.charts.client.controls.filter.DateRangeFilterOptions;
+import com.googlecode.gwt.charts.client.controls.filter.DateRangeFilterUi;
+import com.googlecode.gwt.charts.client.format.DateFormatOptions;
+import com.googlecode.gwt.charts.client.table.Table;
+
+
+import amdb.shared.MovieCollection;
+
+/**
+ * This class defines a static method to draw a <tt>DateRangeFilter</tt> visualizing a <tt>MovieCollection</tt>.
+ * @author selinfabel
+ * @history 2015-12-06
+ * @version 2015-12-06
+ * @responsibilities Provides a method to draw a {@link DateRangeFilter} containing a {@link MovieCollection}
+ * @see DateRangeFilter
+ * @see MovieCollection
+ *
+ */
+public class DateRangeFilter {
+
+	/**
+	 * Draws a <tt>DateRangeFilter</tt> with certain options containing a <tt>MovieCollection</tt>.
+	 * @param dateRangeFilter
+	 * @param collection
+	 */
+	public static void draw(DateRangeFilter dateRangeFilter, MovieCollection collection){
+		
+		//set control options
+//		DateRangeFilterOptions dateRangeFilterOptions = DateRangeFilterOptions.create();
+//		dateRangeFilterOptions.setFilterColumnLabel("Year");
+//		DateRangeFilterUi dateRangeFilterUi = DateRangeFilterUi.create();
+//		dateRangeFilterUi.setFormat(DateFormatOptions.create("yyyy"));
+//		dateRangeFilterOptions.setUi(dateRangeFilterUi);
+//		dateRangeFilter.setOptions(dateRangeFilterOptions);
+//		
+		
+		
+
+	}
+}
